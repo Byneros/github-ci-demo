0e4654e4c6e60ddeca64fead6ac2a3d94034813c
Byneros <ing.nelsonjulio@gmail.com>
2023-07-01 11:13:12 -0500

    Tercer commit

--- HolaMundo.js
+++ HolaMundo.js
@@ -2,4 +2,9 @@ const express = require('express');
 const holamundo = express();
 holamundo.get('/', (req, res) =>
     res.send('Mi Primer Commit!'));
+
+holamundo.get('/test', (req, res) =>
+    res.send('Mi Primer Commit!'));
+
+
 module.exports = holamundo;
